2c56e21245dc2f1c45850688d213be9ed5c93c8f
Rafael Botaro da Silva <rafaelbotaro@yahoo.com.br>
2026-02-27 22:07:45 -0300

    Adicionando create, update and delete of Agenda

--- TCC/src/main/java/com/moduloAi/TCC/controller/AgendaController.java
+++ TCC/src/main/java/com/moduloAi/TCC/controller/AgendaController.java
@@ -1,14 +1,10 @@
 package com.moduloAi.TCC.controller;
 
 import com.moduloAi.TCC.domain.Agenda;
+import com.moduloAi.TCC.dto.AgendaRequest;
 import com.moduloAi.TCC.dto.AgendaResponse;
 import com.moduloAi.TCC.service.AgendaService;
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,7 +25,23 @@ public class AgendaController {
         return agendaService.list();
     }
 
+    @GetMapping("/getAgendaById")
     public Optional<Agenda> getAgendaById(@PathVariable long id){
         return agendaService.getAgendaById(id);
+    }
+
+    @PostMapping("/create")
+    public Agenda create(@RequestBody AgendaRequest agendaRequest){
+        return agendaService.create(agendaRequest);
+    }
+
+    @PutMapping("/update")
+    public Agenda update(@RequestBody AgendaResponse agendaResponse){
+        return agendaService.update(agendaResponse);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable long id){
+        agendaService.delete(id);
     }
 }
